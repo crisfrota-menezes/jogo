4105d5fb4dbf9f8331f45f680dcd1af4fcca4dbb
Cris2021UT <112227932+crisfrota-menezes@users.noreply.github.com>
2023-05-20 00:23:39 -0300

    Update inimigo.cpp
    Implementando o movimento aleatório do inimigo.

--- inimigo.cpp
+++ inimigo.cpp
@@ -20,26 +20,32 @@ void Inimigo::inicializa()
 void Inimigo::perseguir(sf::Vector2f posJogador, sf::Vector2f posInimigo)
 {
     if (posJogador.x - posInimigo.x > 0.0f)
-    {
         corpo.move(vel.x, 0.0f);
-    }
     else
-    {
         corpo.move(-vel.x, 0.0f);
-    }
-
     if (posJogador.y - posInimigo.y > 0.0f)
-    {
         corpo.move(0.0f, vel.y);
-    }
     else
-    {
         corpo.move(0.0f, -vel.y);
-    }
 }
 
 void Inimigo::movimentoAleatorio()
 {
+    if (moveAleatorio == 0)
+        corpo.move(vel.x, 0.0f);
+    else if (moveAleatorio == 1)
+        corpo.move(-vel.x, 0.0f);
+    else if (moveAleatorio == 2)
+        corpo.move(0.0f, vel.y);
+    else
+        corpo.move(0.0f, -vel.y);
+
+    float dt = relogio.getElapsedTime().asSeconds();
+    if (dt >= 1.0f)
+    {
+        moveAleatorio = rand() % 4;
+        relogio.restart();
+    }
 }
 
 void Inimigo::move()
